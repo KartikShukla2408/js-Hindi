887fa51d7a9ecf14e4014353d717e5937f1bd71b
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-05 12:52:34 +0530

    Create var.js
    Variables

---
+++ var.js
@@ -0,0 +1,27 @@
+const accountId = 'in0024nq';
+var accountName = "Kartik Shukla";
+let accountCity = "Kanpur"
+accountPass = "qwerty"
+accountState = "UP";
+
+// console.table([accountCity , accountId , accountName , accountPass]);
+
+// accountId = "in0024nr";
+accountName = "Deepanshi tiwari";
+accountCity = "Noida"
+accountPass = "54321";
+
+// console.table([accountCity , accountId , accountName , accountPass, accountState]);
+console.log(accountCity);
+console.log(accountId);
+console.log(accountName);
+console.log(accountPass)
+console.log(accountState)
+
+// Prefer not to use VAR //
+
+
+
+
+
+
