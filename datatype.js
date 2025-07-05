e5304cd8ef0813a35a9e0474b9bfdd8cf7677679
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-05 12:50:34 +0530

    Add files via upload
    Data types

---
+++ datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // Strict mode to use newer version code of JS
+
+// data types
+
+// alert('Hello');
+
+// string ==> " " or ' ' ;
+// number ==> 23;
+// BigInt ==> number exceeding number datatype Range;
+// Boolean ==> true or false ;
+// null ==> datatype & standalone value;
+// undefined ==>
+// Symbol ===>
+// Object==>
+
+    // let name1 ="Kartik Shukla"
+
+    // console.log(typeof name1); this is used to find the data type of the variable that we have declred.
+
+    let name1 = "Kartik";
+    let age = 23;
+    let genZ=1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+    let loggedIn = false;
+    let Bonus = null;
+    let Salary;
+
+    console.log(typeof name1); //String
+    console.log(typeof age); // number
+    console.log(typeof genZ); // number
+    console.log(typeof loggedIn); // boolean
+    console.log(typeof Bonus); // object
+    console.log(typeof Salary); // undefined
