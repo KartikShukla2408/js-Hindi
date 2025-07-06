2504b44057aea5a5ae1b09c945b94b374c4cc1c1
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-06 08:12:37 +0530

    Add files via upload
    type conversions

---
+++ conversion.js
@@ -0,0 +1,56 @@
+// let age="33abz";
+
+// console.log(typeof age); //String
+// console.log(typeof(age)); // String
+
+// let ageInNum= Number(age);
+
+// console.log(typeof ageInNum); // type conversion from String to Number
+// console.log(ageInNum); // NaN (Not a Number)
+
+// let name1;
+// console.log(typeof name1); // undefined
+// console.log(name1); // undefined
+
+// let newName = Number(name1);
+// console.log(newName); // NaN
+// console.log(typeof newName); // number
+
+
+// let temp = null;
+
+// console.log(temp)// null
+// console.log(typeof temp)// object
+
+// let temp1 = Number(temp)
+
+// console.log(temp1) // 0
+// console.log(typeof temp1) // number
+
+
+// let num = 33;
+// console.log(num); // 33
+// console.log(typeof num); //number
+
+// let num1 = String(num)
+// console.log(num1) // 33
+// console.log(typeof num1) // String
+
+// let name1 = "Kartik"
+// console.log(name1)  // Kartik
+// console.log(typeof name1)   // String
+
+// let name2 = Number(name1); // Type conversion from String to Number
+// console.log(name2)   // NaN
+// console.log(typeof name2)  // number
+
+
+let loggedIn = 1;
+
+console.log(loggedIn)
+console.log(typeof loggedIn)
+
+let check = Boolean(loggedIn);
+console.log(check)
+console.log(typeof check)
+
