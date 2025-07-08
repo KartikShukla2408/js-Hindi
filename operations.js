1d898afeb1ad495cf0bd10ffaba30c968926a756
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-08 11:46:10 +0530

    Add files via upload
    comparison learned

---
+++ operations.js
@@ -0,0 +1,29 @@
+// let value = 33;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// let str = "Kartik";
+// let str1 = " Shukla";
+// let str2 = str + str1;
+// console.log(str2);
+
+// console.log(1 + 2); // number 3
+// console.log(1 + "2"); // numnber 12
+// console.log("1" + 2); // string  12
+// console.log("1" + 2+ 3); // string 123
+// console.log(1 + 2 + "3"); // number 33
+// console.log(1 + "2" + 3); // number 123
+// console.log(2+"3"-4*5/6%7); 
+
+// let a = 3;
+
+// let b = 3;
+
+// console.log(++a); // in this value will increase first then gets print.
+// console.log(b++); // in this the value will get print then it will be increased.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
