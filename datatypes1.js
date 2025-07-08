c4c5f972a1d00a7df105c8aced371473f86c478d
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-08 12:37:33 +0530

    Add files via upload
    Primitive & Non-Primitive Data types learned

---
+++ datatypes1.js
@@ -0,0 +1,64 @@
+// There are 2 types of datatypes in JS
+// Primitive (call by value) & Non-Primitive or (refferece type)
+
+// #primitive                                                          # Non-Primitive
+
+// 1- String                                                          1- Array
+// 2- Number                                                          2- Objects
+// 3- Boolean                                                         3- Function
+// 4- null
+// 5- undefined
+// 6- Symbol ( to make a value unique)
+// 7- BigInt (scientific values)
+
+// JavaScript is a dynamically typed language. This means that the type of a variable is checked during runtime, not at compile time.
+
+// let name1 = "Kartik"
+// let age = 23;
+// let loggedIn = true;
+// let temp = null;
+// let salary;
+// let id = Symbol('123');
+// let num = 343534535443n
+
+
+// console.log(name1)
+// console.log(typeof name1)
+// console.log(age)
+// console.log(typeof age)
+// console.log(loggedIn)
+// console.log(typeof loggedIn)
+// console.log(temp)
+// console.log(typeof temp)
+// console.log(salary)
+// console.log(typeof salary)
+// console.log(id)
+// console.log( typeof id)
+// console.log(num)
+// console.log(typeof num)
+
+let emp = ['Kartik','Shukla', 28,'in0024nq' , null , ' ']
+
+// console.log(emp);
+// console.log(typeof emp);
+
+let emp1 =
+{
+    name:"Pikachu",
+    age: 23,
+    id:'in0024nr',
+    salary: undefined
+}
+
+// console.log(emp1);
+
+let myFuc = Function ()
+{
+    name:"Pikachu";
+    age: 23;
+    id:'in0024nr';
+    salary: undefined
+}
+
+console.log( typeof myFuc);
+ 
