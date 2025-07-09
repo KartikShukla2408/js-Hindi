fafdf6df3117201eac0d3c29370d782b19356696
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-09 12:26:47 +0530

    Add files via upload
    String and its methods

---
+++ String.js
@@ -0,0 +1,67 @@
+// let name = "Kartik";
+// let age = 23;
+
+// let empID = (name + age); 
+
+// console.log(empID);
+
+// This is old way of concatinating the strings.
+
+
+// console.log(`Hello my name is ${name} and my age is ${age} and my employeeiD is ${name + age}`);
+
+// this is the new way of concatinating the strings which is also known as String interpolation.
+
+// let game = new String('Automatically'); // this is another way of declaring string.
+
+// console.log(game);
+
+// String Menthods.
+
+
+// console.log(game.length); // this is will print the lenght
+
+// console.log(game.charAt(4)); // this will print the character which is present at the position that we mention.
+
+// console.log(game.endsWith);  // check 
+
+// console.log(game.includes ('k')); // check if string containes the character that we have mentioned.
+
+// console.log(game.toLocaleLowerCase());
+
+// console.log(game.toLocaleUpperCase());
+
+// console.log(game.lastIndexOf()); // last position of the string
+
+// console.log(game.indexOf('y')); // index of that char
+
+// console.log(game.substring(2,6));  // this will slice the string from starting index to -1 position
+
+// difference between slice and sub string is in slice we can use negative value.
+
+
+// console.log(game.slice(-8 , 5));
+
+
+// let learnTrim = "    Kartik Shukla      "
+
+// console.log(learnTrim);
+
+// console.log(learnTrim.trim());
+
+// let url = "https://kartik.shukla%20";
+
+// console.log(url.replace('%20' , 'pikachu'));
+
+// console.log(url.includes('pikachu'));
+
+
+let pikachu = "Kartik Shukla Deepanshi Tiwari";
+
+console.log(pikachu.split(' '));
+
+console.log(pikachu.startsWith('K'));
+
+console.log(pikachu.bold());
+
+
