63736c9af781c87fa66b1ffcc22fd7f2953f280f
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-09 11:20:05 +0530

    Add files via upload
    Stack & Heap memory management

---
+++ Stack_heap.js
@@ -0,0 +1,38 @@
+// Stack is used in all primitive datatypes.
+
+// Heap memory is used in all non-promitive datatypes.
+
+// let name1 = "Kartik"
+// let full_name = name1;
+// full_name = "Kartik Shukla";
+// console.log(full_name);
+// console.log(name1);
+
+// let emp1_age = 23;
+
+// let emp2_age = emp1_age;
+// emp2_age = emp1_age + 2;
+// console.log(emp1_age);
+// console.log(emp2_age);
+
+let logIn = true;
+let signIn = logIn;
+signIn = false;
+
+console.log(logIn)
+console.log(signIn)
+
+
+
+
+// let userOne = {
+//     email : "Kartik@blabla.com",
+//     upi: "golu@ybl"
+// }
+
+// let userTwo= userOne;
+
+// userTwo.email = "Kartik.shukla@tululu.com";
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
