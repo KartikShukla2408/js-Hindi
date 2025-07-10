321923cd1383c34e499b949e0a0676383dae74ff
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-10 11:49:13 +0530

    Add files via upload
    Learn about number, Maths and its methods.

---
+++ Num_Maths.js
@@ -0,0 +1,31 @@
+// const score = 400;
+// console.log(score);
+// const new_score = new Number(405); // another way of declaring the number
+// console.log(new_score);
+// console.log(new_score.toString()) // this will turn our number to string.
+// console.log(new_score.toFixed(2)) // 100.00 this kind of result will come.
+// const price = 456.487;
+// console.log(price.toPrecision(4));
+// const zeros = 10000000000000;
+// console.log(zeros.toLocaleString('en-IN'));
+
+///////////////////////////////////////////////MATHS///////////////////////////////////////////////////////////////
+
+
+
+// console.log(Math.abs(-23));  // this will turn -ve number to +ve
+// console.log(Math.round(23.4)); // this will round off number
+// console.log(Math.ceil(42.1)); // this will round off number but will give +1 value 
+// console.log(Math.floor(42.9)); // this will give the floor value
+// console.log(Math.sqrt(25));  // this will find sq root
+// console.log(Math.min(12,23,45,78)); // min value
+// console.log(Math.max(45,65,78,3453)); // max value
+
+
+// console.log(Math.random()); // this will give randon number which range lies between 0 to 1
+
+// console.log(Math.floor(Math.random() * (max - min +1 )) + min)  this formula will min to max value
+
+
+
+
