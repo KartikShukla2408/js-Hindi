d9dd6f73aee86d85388c2831e54d7d7841760513
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-12 09:50:11 +0530

    Add files via upload
    Learn about date & time

---
+++ Date_time.js
@@ -0,0 +1,16 @@
+let Mydate = new Date();
+// console.log(Mydate.toString());
+// console.log(Mydate.toDateString());
+// console.log(Mydate.toISOString())
+// console.log(Mydate.toJSON())
+// console.log(Mydate.toLocaleDateString())
+// console.log(Mydate.toLocaleString())
+// console.log(Mydate.toTimeString())
+//////////////////////////////////////////// After new launch /////////////////////////////////////////////////
+let myNewDate = new Date(2025 , 6, 11);
+console.log(myNewDate.getFullYear());
+console.log(myNewDate.getDate());
+console.log(myNewDate.getDay());
+console.log(myNewDate.getHours());
+
+
