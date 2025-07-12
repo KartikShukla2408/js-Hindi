4726097d2ddfbfc0ee71f5407f22b1ddb2bb76ac
KartikShukla2408 <97380178+KartikShukla2408@users.noreply.github.com>
2025-07-12 10:48:15 +0530

    Add files via upload
    Learn about arrays and its basic operations

---
+++ Arrays.js
@@ -0,0 +1,67 @@
+
+///////////////////////////////// These are the ways to declare Arrays ///////////////////////////////////////////
+
+// const arr = [1,2,3,4,5, true ,'Kartik'];
+
+// console.log(arr[3]);
+
+// let arr1 = ["Spiderman" , 'Batman'];
+
+// console.log(arr1[1]);
+
+// let arr2 = [1,2,3,4,5];
+
+// console.log(arr[3]);
+
+// let myArr = new Array(0,9,8,7,6);
+
+// console.log(myArr[4]);
+
+
+////////////////////////////////////// Array Methods /////////////////////////////////////////////////////
+
+
+let myArr = new Array (1,2,3,4);
+// console.log(myArr);
+
+// myArr.push(8);
+// console.log(myArr);  // To add a element of the array.
+
+// myArr.pop();
+// console.log(myArr); // To delete the last element of the array.
+
+
+// console.log(myArr.length);  // To find the length of that array.
+
+// myArr.unshift(9);
+// console.log(myArr); // this will add the value the we will pass at the start of the array.
+
+// myArr.shift();
+// console.log(myArr);  // this will remove an element from array but from the start not from end.
+
+// console.log(myArr.includes(9)); // this will check if the array includes the element that we are looking for.
+
+// console.log(myArr.indexOf(2)); // this will give the index of the element we are searching for.
+
+
+// let newarr = myArr.join() // this join method will convert array into in string.
+
+// console.log(myArr);
+// console.log( typeof newarr); 
+
+
+// console.log(myArr.slice(1,3)); // this will start from 1 and end at 2 but will not touch the last and also not manupilate the array
+// console.log(myArr);
+
+// console.log(myArr.splice(1,3)); // this will include the last value and will also manupilate the array.
+// console.log(myArr);
+
+
+
+
+
+
+
+
+
+
